c69b8cca9ca9a8fccb1300cd93553cfc7ac2b8dd
Zowie <git@zowi.ee>
2022-02-16 00:30:20 +0100

    Add constants for various scripts

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const SCRIPT_WEAKEN = "cmd-weaken.js"
+export const SCRIPT_HACK = "cmd-hack.js"
+export const SCRIPT_GROW = "cmd-grow.js"
+export const SCRIPT_WGWH_LOOP = "wgwh-loop.js"
+export const SCRIPT_WGWH_STATUS = "wgwh-status.js"
+export const SCRIPT_SERVER_STATUS = "server-status.js"
