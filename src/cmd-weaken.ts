5268b895ccbf0f2251b051f0f6f1fd561960c9aa
Zowie <git@zowi.ee>
2022-02-16 00:34:23 +0100

    [cmd-*] Better output and optional delay

--- src/cmd-weaken.ts
+++ src/cmd-weaken.ts
@@ -3,18 +3,21 @@ import { NS } from '@ns'
 interface Flags {
   target: string
   threads: number
+  delay: number
 }
 
 export async function main(ns : NS) : Promise<void> {
   const flags = ns.flags([
     ["target", ""],
     ["threads", 1],
+    ["delay", 0],
   ]) as Flags;
 
   if (flags.target.length === 0) {
-    ns.print("ERROR: No target specified");
+    ns.tprintf("ERROR: %s - %s No target specified", ns.getHostname(), ns.getScriptName());
     return
   }
 
+  await ns.asleep(flags.delay)
   await ns.weaken(flags.target, { threads: flags.threads });
 }
