e6d47a59afe0576522efd488ff12885895291c09
Zowie <git@zowi.ee>
2022-02-16 00:32:25 +0100

    [killall] clean up and improve output

--- src/killall.ts
+++ src/killall.ts
@@ -3,11 +3,19 @@ import scanHost from '/lib-scan-host'
 
 export async function main(ns : NS) : Promise<void> {
   const hosts = scanHost(ns);
+  let totalKilled = 0;
   for (const host in hosts) {
-    const procs = ns.ps(host).length;
-    if (procs > 0) {
-      ns.killall(host);
-      ns.print(ns.sprintf("Killed %d processes on %s", procs, host));
+    const procs = ns.ps(host).filter(v => v.filename !== "killall.js");
+    if (procs.length === 0) {
+      continue;
     }
+
+    for (const proc of procs) {
+      totalKilled += ns.kill(proc.pid) ? 1 : 0
+    }
+
+    ns.tprintf("Killed %d processes on %s", procs.length, host);
   }
+
+  ns.tprintf("Killed %s total processes!", ns.nFormat(totalKilled, "0,0"))
 }
