d643071669dfe1fd52a14019678be59174f2ced6
Zowie <git@zowi.ee>
2022-03-17 13:44:17 +0100

    Add number formatting utility functions

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -19,3 +19,54 @@ export function filterUndefinedFunc<T>(): (
 ) => value is T {
   return (value: T | undefined, _index: number, _array: (T | undefined)[]): value is T => value !== undefined
 }
+
+export function formatNum(ns: NS, value: number, fmt = "0,0.00"): string {
+  return ns.nFormat(value, fmt).replaceAll(",", " ")
+}
+
+export function formatMoney(ns: NS, value: number, fmt = "$0,0.00a"): string {
+  return formatNum(ns, value, fmt)
+}
+
+export function formatBytes(ns: NS, value: number, fmt = "0,0.00ib"): string {
+  return formatNum(ns, value, fmt)
+}
+
+export function formatGiB(ns: NS, value: number, fmt = "0,0.00ib"): string {
+  return formatBytes(ns, value * 1024 ** 3, fmt)
+}
+
+const MillisecondsPerDay = 24 * CONSTANTS.MillisecondsPerHour
+const MillisecondsPerMinute = 60 * 1_000
+export function formatTime(ms: number, showMs = false): string {
+  const days = Math.floor(ms / MillisecondsPerDay)
+  const msTruncDays = ms % MillisecondsPerDay
+
+  const hours = Math.floor(msTruncDays / CONSTANTS.MillisecondsPerHour)
+  const msTruncHours = msTruncDays % CONSTANTS.MillisecondsPerHour
+
+  const minutes = Math.floor(msTruncHours / MillisecondsPerMinute)
+  const msTruncMinutes = msTruncHours % MillisecondsPerMinute
+
+  const seconds = Math.floor(msTruncMinutes / 1_000)
+  const msTruncSeconds = Math.round(msTruncMinutes % 1_000)
+
+  if (days > 0) {
+    return days + "d" + hours + "h" + minutes + "m" + seconds + "s" + (showMs ? msTruncSeconds + "ms" : "")
+  }
+
+  if (hours > 0) {
+    return hours + "h" + minutes + "m" + seconds + "s" + (showMs ? msTruncSeconds + "ms" : "")
+  }
+
+  if (minutes > 0) {
+    return minutes + "m" + seconds + "s" + (showMs ? msTruncSeconds + "ms" : "")
+  }
+
+  if (seconds > 0) {
+    return seconds + "s" + (showMs ? msTruncSeconds + "ms" : "")
+  }
+
+  return msTruncSeconds + "ms"
+}
+
