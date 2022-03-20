05d7fb203744a5c29a93bd420b102be1caa74ca8
Zowie <git@zowi.ee>
2022-03-20 19:05:35 +0100

    remove obsolete script

--- src/start-hack.ts
+++
@@ -1,8 +0,0 @@
-import { NS } from "@ns"
-import setupPolyfill from "/lib/ns-polyfill"
-
-export async function main(ns: NS): Promise<void> {
-  setupPolyfill(ns)
-
-  ns.spawn("start.js", 1, "--hack")
-}
