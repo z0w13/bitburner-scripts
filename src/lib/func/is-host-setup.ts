182e23801668b829dcd27c78afe282187653f6fd
Zowie <git@zowi.ee>
2022-06-15 12:02:40 +0200

    Don't check for root in isHostSetup as we can use hosts without root

--- src/lib/func/is-host-setup.ts
+++ src/lib/func/is-host-setup.ts
@@ -2,8 +2,5 @@ import { NS } from "@ns"
 import { COPY_SCRIPTS } from "/constants"
 
 export default function isHostSetup(ns: NS, hostname: string): boolean {
-  const haveRoot = ns.hasRootAccess(hostname)
-  const scriptsInstalled = COPY_SCRIPTS.map((s) => ns.fileExists(s, hostname)).indexOf(false) === -1
-
-  return haveRoot && scriptsInstalled
+  return COPY_SCRIPTS.map((s) => ns.fileExists(s, hostname)).indexOf(false) === -1
 }
