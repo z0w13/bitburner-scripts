be4e07ec06c59cfb70c7e9ebfe2970616fef1f7b
Zowie <git@zowi.ee>
2022-06-15 12:01:31 +0200

    Add a flag to farm karma to do-crime

--- src/do-crime.ts
+++ src/do-crime.ts
@@ -3,8 +3,14 @@ import setupPolyfill from "/lib/ns-polyfill"
 import { getBestCrime } from "/data/Crimes"
 import getPlayerAction, { PlayerActionType } from "/lib/func/get-player-action"
 
+interface Flags {
+  karmawhore: boolean
+}
+
 export async function main(ns: NS): Promise<void> {
   setupPolyfill(ns)
+
+  const flags = ns.flags([["karmawhore", false]]) as Flags
 
   ns.disableLog("ALL")
   ns.enableLog("commitCrime")
@@ -13,6 +19,11 @@ export async function main(ns: NS): Promise<void> {
     await ns.asleep(1000)
     const action = getPlayerAction(ns)
     if (action.type === PlayerActionType.Crime) {
+      continue
+    }
+
+    if (flags.karmawhore) {
+      ns.singularity.commitCrime("Homicide")
       continue
     }
 
