55358524ba8389731190d59efa544c7a95f97c54
Zowie <git@zowi.ee>
2023-01-02 21:06:46 +0100

    chore: update deps

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,9 @@
-import _ from 'lodash'
 import * as bitburner from "./NetscriptDefinitions";
 import { GlobalState } from "/lib/shared/GlobalStateManager";
 
 export { };
 
 declare global {
-    const _: typeof _
-
     interface NS extends bitburner.NS {}
 
     type AutocompleteConfig = [string, string | number | boolean | string[]][];
