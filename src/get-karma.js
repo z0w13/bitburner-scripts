dd052bcc452386260801de7f2b3204d2ea04de8b
Zowie <git@zowi.ee>
2023-02-12 16:48:40 +0100

    chore: remove unused files

--- src/get-karma.js
+++
@@ -1,8 +0,0 @@
-export async function main(ns) {
-  ns.disableLog("ALL")
-
-  while (true) {
-    await ns.asleep(1000)
-    ns.heart.break()
-  }
-}
--- src/lib/DecisionTree.ts
+++
@@ -1,9 +0,0 @@
-export type Outcomes = [() => void, () => void]
-
-export function decide(decision: boolean, outcomes: Outcomes): void {
-  if (decision) {
-    outcomes[0]()
-  } else {
-    outcomes[1]()
-  }
-}
