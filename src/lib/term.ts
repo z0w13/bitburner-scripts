7a9ed8589e913b098330ef478133e2112f0a5c11
Zowie <git@zowi.ee>
2023-03-30 16:06:57 +0200

    feat: add formatChangeColor to term utilities, changes color of displayValue based on input value

--- src/lib/term.ts
+++ src/lib/term.ts
@@ -23,6 +23,23 @@ export function wrapColor(color: TermColor, val: string): string {
   return color + val + TermColor.Reset
 }
 
+export function formatChangeColor(value: number | boolean, display: unknown, thresh = 0): string {
+  const renderVal = display !== null ? String(display) : String(value)
+
+  if (typeof value === "boolean") {
+    return wrapColor(value ? TermColor.Green : TermColor.Red, renderVal)
+  }
+
+  if (value > thresh) {
+    return wrapColor(TermColor.Green, renderVal)
+  }
+  if (value < -thresh) {
+    return wrapColor(TermColor.Red, renderVal)
+  }
+
+  return renderVal
+}
+
 export function ansiRegex(): RegExp {
   const pattern = [
     "[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)",
