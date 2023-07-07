6f05bfebeab113c5f3432c5fe78407086d5e1af5
Zowie <me@zowie.cx>
2023-07-07 19:37:01 +0200

    fix(trader): don't start already running scripts

--- src/start-trader.ts
+++ src/start-trader.ts
@@ -7,6 +7,7 @@ import {
   STOCK_TRADER_SCRIPT,
 } from "@/StockTrader/constants"
 import { DAEMON_SERVER } from "@/config"
+import getScriptPid from "@/lib/func/get-script-pid"
 import tailExec from "@/lib/func/tail-exec"
 import parseFlags from "@/lib/parseFlags"
 import { NS } from "@ns"
@@ -21,14 +22,19 @@ export async function main(ns: NS): Promise<void> {
     STOCK_TRACKER_SCRIPT,
     STOCK_TRADER_SCRIPT,
     STOCK_LOG_SCRIPT,
-    STOCK_HUD_SCRIPT,
     STOCK_COORDINATOR_SCRIPT,
   ]
 
   scripts.forEach((script) => {
-    if (!tailExec(ns, script, DAEMON_SERVER, 1, ...args)) {
+    if (!getScriptPid(ns, script, DAEMON_SERVER) && !ns.exec(script, DAEMON_SERVER, 1, ...args)) {
       ns.tprint(`ERROR: Couldn't start ${script}`)
-      return
     }
   })
+
+  if (
+    !getScriptPid(ns, STOCK_HUD_SCRIPT, DAEMON_SERVER) &&
+    !tailExec(ns, STOCK_HUD_SCRIPT, DAEMON_SERVER, 1, ...args)
+  ) {
+    ns.tprint(`ERROR: Couldn't start ${STOCK_HUD_SCRIPT}`)
+  }
 }
