771eaf963f40f6c1c48eab5ecaca470262a086ff
Zowie <me@zowie.cx>
2023-07-07 18:32:36 +0200

    fix: call to determine current bitnode

--- src/PlayerManager/Actions/InstallAugmentsAction.ts
+++ src/PlayerManager/Actions/InstallAugmentsAction.ts
@@ -4,7 +4,7 @@ import BaseAction from "@/PlayerManager/Actions/BaseAction"
 export default class InstallAugmentsAction extends BaseAction {
   shouldPerform(ns: NS): boolean {
     const newAugs = ns.singularity.getOwnedAugmentations(true).length - ns.singularity.getOwnedAugmentations().length
-    return newAugs >= 10 || (ns.getPlayer().bitNodeN === 4 && newAugs >= 3)
+    return newAugs >= 10 || (ns.getResetInfo().currentNode === 4 && newAugs >= 3)
   }
 
   isPerforming(_ns: NS): boolean {
