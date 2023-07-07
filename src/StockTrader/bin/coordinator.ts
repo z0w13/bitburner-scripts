f195a9391a16931369f4bfc8cc8679ac807ef0ff
Zowie <me@zowie.cx>
2023-07-07 19:33:36 +0200

    fix: respect mock flag in coordinator

--- src/StockTrader/bin/coordinator.ts
+++ src/StockTrader/bin/coordinator.ts
@@ -2,15 +2,18 @@ import { NS } from "@ns"
 
 import { getAnalyserPidOrStart } from "@/StockTrader/lib/Analyser"
 import { getTrackerPidOrStart } from "@/StockTrader/lib/Tracker"
+import parseFlags from "@/lib/parseFlags"
 
 export async function main(ns: NS): Promise<void> {
+  const flags = parseFlags(ns, { mock: false })
+
   // Wait for a bit to give scripts a chance to start
   await ns.asleep(1000)
 
   // Ensure the analyser/tracker are running
   while (true) {
-    getAnalyserPidOrStart(ns)
-    getTrackerPidOrStart(ns)
+    getAnalyserPidOrStart(ns, flags.mock)
+    getTrackerPidOrStart(ns, flags.mock)
 
     await ns.asleep(5000)
   }
