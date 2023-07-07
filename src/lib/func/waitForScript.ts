b1aa7e14df7d4eac87029b4f98909621eed508d4
Zowie <me@zowie.cx>
2023-07-07 19:32:59 +0200

    fix: waitForScript timeouts

--- src/lib/func/waitForScript.ts
+++ src/lib/func/waitForScript.ts
@@ -3,7 +3,8 @@ import { NS } from "@ns"
 
 export async function waitForScript(ns: NS, name: string, host: string, timeout = 0): Promise<number> {
   const startTime = Date.now()
-  while (timeout === 0 || startTime + timeout * 1000 < Date.now()) {
+
+  while (timeout === 0 || startTime + timeout * 1000 > Date.now()) {
     const pid = getScriptPid(ns, name, host)
     if (pid > 0) {
       return pid
