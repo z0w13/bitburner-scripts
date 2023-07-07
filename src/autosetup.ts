076952e52b240f3f5d7553604195c0c533178085
Zowie <me@zowie.cx>
2023-07-07 18:52:27 +0200

    feat: auto-backdooring in autosetup

--- src/autosetup.ts
+++ src/autosetup.ts
@@ -1,4 +1,4 @@
-import type { NS } from "@ns"
+import { type NS } from "@ns"
 import getHosts from "@/lib/func/get-hosts"
 import hackHost from "@/lib/func/hack-host"
 import isHostSetup from "@/lib/func/is-host-setup"
@@ -6,6 +6,8 @@ import { COPY_SCRIPTS } from "@/constants"
 import { DAEMON_SERVER } from "@/config"
 import rsync from "@/lib/func/rsync"
 import parseFlags from "@/lib/parseFlags"
+import getHostPath from "@/lib/func/get-host-path"
+import { FactionJoinRequirementData } from "@/data/Factions"
 
 export async function main(ns: NS): Promise<void> {
   ns.disableLog("ALL")
@@ -16,10 +18,29 @@ export async function main(ns: NS): Promise<void> {
     scripts: COPY_SCRIPTS, // Scripts to install
   })
 
+  const backdoorHosts = Object.values(FactionJoinRequirementData)
+    .map((d) => d.backdoor)
+    .filter<string>((d): d is string => d !== undefined)
+
   while (true) {
     const hosts = getHosts(ns)
     for (const hostname of hosts) {
       hackHost(ns, hostname)
+      const server = ns.getServer(hostname)
+
+      if (!server.backdoorInstalled && backdoorHosts.includes(hostname) && server.hasAdminRights) {
+        try {
+          const path = getHostPath(ns, hostname)
+          if (path) {
+            path.forEach((p) => ns.singularity.connect(p))
+            await ns.singularity.installBackdoor()
+            ns.toast(`Backdoored ${hostname}`, "success")
+            ns.singularity.connect("home")
+          }
+        } catch (e) {
+          ns.tprint("ERR: ", e)
+        }
+      }
 
       if (isHostSetup(ns, hostname)) {
         continue
